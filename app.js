486702b23d147d787215ac9515b158391cd160b9
jaynedoe <jaedoyne@hotmail.com>
2021-03-09 10:22:13 +1100

    built index.html grid and css styling

---
+++ app.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const allSquares = document.querySelectorAll(".grid div");
+    const scoreDisplay = document.querySelector("#score");
+    const startBtn = document.querySelector("#start");
+
+
+
+    
+
+
+
+
+
+
+
+});
